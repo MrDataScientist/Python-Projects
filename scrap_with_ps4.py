aa82648e16fe3a8a86129837e26b20c38e74433b
Tarik EN NAKDI <t.ennakdi@gmail.com>
2016-05-30 19:05:20 +0200

    Create scrap_with_ps4.py

---
+++ scrap_with_ps4.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url= ""
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content)
+
+links = soup.find_all("a")
+
+for link in links:
+	print " href="  link.text)

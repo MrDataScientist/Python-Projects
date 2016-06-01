7fc03047a5953269d10c2f06be94220b4fff0611
Tarik EN NAKDI <t.ennakdi@gmail.com>
2016-06-01 18:55:39 +0200

    Update scrap_with_ps4.py
    Scrape Websites with Python + Beautiful Soup 4 + Requests -- Coding with Python

--- scrap_with_ps4.py
+++ scrap_with_ps4.py
@@ -1,3 +1,4 @@
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -9,5 +10,16 @@ soup = BeautifulSoup(r.content)
 links = soup.find_all("a")
 
 for link in links:
-	print " href="  link.get("href"), link.text)
+	print " href="  %link.get("href"), link.text)
 g_data = soup.find_all("div")
+
+for item in g_data:
+	print item.contents[0].find_all("a",{"class":"business-name"})[0].text
+	print item.contents[1].find_all("p",{"class":"adr"})[0].text
+	try:
+	print item.contents[1].find_all("span",{"itemprop":"address"})[0].text
+	print item.contents[1].find_all("span",{"class":"addressLocality"})[0].text
+	try:
+		print item.contents[1].find_all("li",{"class":"primary"})[0].text
+	except:
+		pass

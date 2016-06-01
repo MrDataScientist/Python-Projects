de631333828aa38e6921b2911e1f4dac3b8b3486
Tarik EN NAKDI <t.ennakdi@gmail.com>
2016-06-01 19:01:04 +0200

    Update scrap_with_ps4.py

--- scrap_with_ps4.py
+++ scrap_with_ps4.py
@@ -2,7 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url= ""
+url= "http://www.yellowpages.com/nashport-oh/mip/coffee-time-office-coffee-service-13629601?lid=13629601"
 r = requests.get(url)
 
 soup = BeautifulSoup(r.content)
@@ -17,9 +17,12 @@ for item in g_data:
 	print item.contents[0].find_all("a",{"class":"business-name"})[0].text
 	print item.contents[1].find_all("p",{"class":"adr"})[0].text
 	try:
-	print item.contents[1].find_all("span",{"itemprop":"address"})[0].text
-	print item.contents[1].find_all("span",{"class":"addressLocality"})[0].text
+		print item.contents[1].find_all("span",{"itemprop":"address"})[0].text
+		print item.contents[1].find_all("span",{"class":"addressLocality"})[0].text
+	except:
+		pass
 	try:
 		print item.contents[1].find_all("li",{"class":"primary"})[0].text
 	except:
 		pass
+

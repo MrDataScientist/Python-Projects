83fb2c9741a2b5fc8bdcf5cfe1b33b95a4a2fd39
Tarik EN NAKDI <t.ennakdi@gmail.com>
2016-06-03 14:27:58 +0200

    Update client.py

--- client.py
+++ client.py
@@ -9,3 +9,14 @@ port = 12345                # Reserve a port for your service.
 s.connect((host, port))
 print s.recv(1024)
 s.close                     # Close the socket when done
+
+--------------------------------------------------
+#!/usr/bin/python           # This is server.py file
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+
